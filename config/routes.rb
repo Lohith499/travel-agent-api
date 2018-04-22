09b24c488dc8401978f046a01a26e8eabf3c7cd7
Lohith499 <lohith.gre@gmail.com>
2018-04-22 07:45:00 -0400

    second

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :vacations do
-  resources :customers
+  resources :customers do
+  resources :vacations
   end
 end
