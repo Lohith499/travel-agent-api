b1e4ae243e9d7f6e3a467a60617d55b7667c0da2
Lohith499 <lohith.gre@gmail.com>
2018-04-22 06:55:57 -0400

    change in db file

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,7 @@
 class Customer < ApplicationRecord
 validates :FirstName, presence: true, length: {minimum: 5, maximum: 25}
 validates :LastName, presence: true, length: {minimum: 5, maximum: 25}
-validates :Phone, presence: true, length: 10
+validates :Phone, presence: true, length: {minimum: 10, maximum: 10}
 validates :Address, presence: true,length: {minimum: 20, maximum: 600}
 validates :TravelAgent_email, presence: true
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
