864a490a6d12e883ed37bbf75897b03ed7edf210
Lohith499 <lohith.gre@gmail.com>
2018-04-22 06:52:21 -0400

    change in db file

--- db/migrate/20180422095638_add_customer_to_vacation.rb
+++ db/migrate/20180422095638_add_customer_to_vacation.rb
@@ -1,5 +1,5 @@
 class AddCustomerToVacation < ActiveRecord::Migration[5.1]
   def change
-    add_reference :vacations, :Customer, foreign_key: true
+    add_reference :vacations, :customer, foreign_key: true
   end
 end
