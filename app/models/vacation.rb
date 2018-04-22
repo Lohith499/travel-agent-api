9ddcc2b3b51f58d6e6d9d62d93ea70b016f7af33
Lohith499 <lohith.gre@gmail.com>
2018-04-22 12:37:35 -0400

    second

--- app/models/vacation.rb
+++ app/models/vacation.rb
@@ -1,4 +1,9 @@
 class Vacation < ApplicationRecord
   enum Transport: [:Airways, :Train, :Bus, :Cab]
+  validates :Place, presence: true, length: {minimun: 5, maximum: 40}
+  validates :Date, presence: true
+  validates :Description, presence: true, length: {minimun: 20, maximum: 600}
+  validates :Budget, presence: true
+  validates :Transport, status: true
   belongs_to :customer
 end
