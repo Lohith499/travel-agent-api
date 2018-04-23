efd8917b7680e8f5bdac8b61f2b4225e56c4afb6
Lohith499 <lohith.gre@gmail.com>
2018-04-23 05:26:14 -0400

    removed email uniqueness

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,7 +6,6 @@ class Customer < ApplicationRecord
   validates :TravelAgent_email, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :TravelAgent_email, length: {maximum: 105},
-        uniqueness: { case_sensative: false },
         format: { with: VALID_EMAIL_REGEX }
 has_many :vacations
 end
