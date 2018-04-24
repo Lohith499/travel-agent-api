d17d2cd205cf6ce53b1759ef14dffd331f696afe
Lohith499 <lohith.gre@gmail.com>
2018-04-24 07:55:50 -0400

    origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://travel-agent-mm.herokuapp.com/'
+    origins '*'
     resource '*',
       headers: :any,
       methods: %i(get post put patch delete options head)
